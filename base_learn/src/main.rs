27426ce3e54660dd4f4a4cf020a3dea53b910a2f
zhenhua32 <zhenhua32@gmail.com>
2024-05-26 14:01:32 +0800

    save

---
+++ base_learn/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const y: &str = "Hello, world!";
+    println!("The value of y is: {}", y);
+}
