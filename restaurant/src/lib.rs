d6720e5e12996c74c1af5d714849504ee116e3d4
zhenhua32 <zhenhua32@gmail.com>
2025-04-05 14:29:24 +0800

    save

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -2,7 +2,50 @@ mod front_of_house;
 
 pub use crate::front_of_house::hosting;
 
+fn deliver_order() {
+    println!("Delivering order!");
+}
+
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {
+        println!("Cooking order!");
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
 pub fn eat_at_restaurant() {
-    hosting::add_to_waitlist();
+    hosting::add_to_waits();
     println!("I'm eating at the restaurant!");
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // meal.seasonal_fruit = String::from("blueberries"); // Error! seasonal_fruit is private
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
