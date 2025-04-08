c8aec7eb013d191dac3f7b3ef9bad059205b38a0
zhenhua32 <zhenhua32@gmail.com>
2025-04-08 21:18:42 +0800

    save

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,16 @@
 use std::env;
 use std::fs;
+use std::process;
 
+// 启动命令: cargo run -- "duct tape" poem.txt
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
@@ -20,6 +25,20 @@ struct Config {
     filename: String,
 }
 
+impl Config{
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        } else if args.len() > 3 {
+            return Err("Too many arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
 
 fn parse_config(args: &[String]) -> Config {
     if args.len() < 3 {
