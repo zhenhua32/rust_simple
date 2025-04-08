72785e99f92bfcad6492ab9935261400d41ff204
zhenhua32 <zhenhua32@gmail.com>
2025-04-08 22:49:48 +0800

    一小步

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -23,6 +23,54 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust: 
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn test_build() {
+        let args = vec![
+            String::from("minigrep"),
+            String::from("duct tape"),
+            String::from("poem.txt"),
+        ];
+        let config = Config::build(&args).unwrap();
+        assert_eq!(config.query, "duct tape");
+        assert_eq!(config.filename, "poem.txt");
+    }
+
+    #[test]
+    fn test_not_enough_arguments() {
+        let args = vec![String::from("minigrep")];
+        let result = Config::build(&args);
+        assert!(result.is_err());
+    }
 }
