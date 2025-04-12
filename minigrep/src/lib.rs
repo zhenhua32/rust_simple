f4aac61d6fe9a3c9204c1724f7c179175d667f88
zhenhua32 <zhenhua32@gmail.com>
2025-04-12 11:17:08 +0800

    save

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -4,28 +4,32 @@ use std::env;
 
 pub struct Config {
     pub query: String,
-    pub filename: String,
+    pub file_path: String,
     pub ignore_case: bool,
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        } else if args.len() > 3 {
-            return Err("Too many arguments");
-        }
+    pub fn build(mut  args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next(); // skip the first argument (the program name)
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, filename, ignore_case })
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
+    let contents = fs::read_to_string(config.file_path)?;
 
     let results = if config.ignore_case {
         search_case_insensitive(&config.query, &contents)
@@ -40,14 +44,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 
@@ -100,7 +99,7 @@ Trust me.";
         ];
         let config = Config::build(&args).unwrap();
         assert_eq!(config.query, "duct tape");
-        assert_eq!(config.filename, "poem.txt");
+        assert_eq!(config.file_path, "poem.txt");
     }
 
     #[test]
