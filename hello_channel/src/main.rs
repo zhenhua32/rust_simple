0ea4b15a06169dbc22094bfcf947617721e0d84a
zhenhua32 <zhenhua32@gmail.com>
2025-04-27 21:46:23 +0800

    save

---
+++ hello_channel/src/main.rs
@@ -0,0 +1,16 @@
+use std::sync::mpsc;
+use std::thread;
+
+fn main() {
+    println!("Hello, world!");
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hello");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
