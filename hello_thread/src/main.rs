d9dc02a615e2d078c012a0332c2c6a247c1a0071
zhenhua32 <zhenhua32@gmail.com>
2025-04-27 21:05:48 +0800

    save

--- hello_thread/src/main.rs
+++ hello_thread/src/main.rs
@@ -2,7 +2,13 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let v = vec![1, 2, 3, 4, 5];
+
+    let handler2 = thread::spawn(move || {
+        println!("Thread received vector: {:?}", v);
+    });
+
+    let handler = thread::spawn(|| {
         for i in 1..10 {
             println!("Hello from thread! {}", i);
             thread::sleep(Duration::from_millis(1));
@@ -15,4 +21,7 @@ fn main() {
         println!("Hello from main thread! {}", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    handler.join().unwrap();
+    println!("Thread finished execution.");
 }
