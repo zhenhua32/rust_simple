33b57be583da5713ac6fcb8fc843c351557d6575
zhenhua32 <zhenhua32@gmail.com>
2025-04-27 21:50:03 +0800

    save

--- hello_channel/src/main.rs
+++ hello_channel/src/main.rs
@@ -1,5 +1,6 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     println!("Hello, world!");
@@ -9,8 +10,18 @@ fn main() {
     thread::spawn(move || {
         let val = String::from("hello");
         tx.send(val).unwrap();
+
+        let vals = vec!["hello", "world", "from", "Rust"];
+        for val in vals {
+            tx.send(val.to_string()).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
     let received = rx.recv().unwrap();
     println!("Got: {}", received);
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
