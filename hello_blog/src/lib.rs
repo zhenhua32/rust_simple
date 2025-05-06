e9d225a9534c70782d5a168de2b579cac45b86ee
zhenhua32 <zhenhua32@gmail.com>
2025-05-06 20:58:00 +0800

    save

--- hello_blog/src/lib.rs
+++ hello_blog/src/lib.rs
@@ -24,24 +24,57 @@ impl Post {
             self.state = Some(s.request_review())
         }
     }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
 }
 
 trait State {
     fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
 }
 
 struct Draft {}
 
 impl State for Draft {
+    // 从 Draft 状态转换到 PendingReview 状态
     fn request_review(self: Box<Self>) -> Box<dyn State> {
         Box::new(PendingReview {})
+    }
+
+    // 没有改变
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
     }
 }
 
 struct PendingReview {}
 
 impl State for PendingReview {
+    // 没有改变
     fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    // 从 PendingReview 状态转换到 Published 状态
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    // 没有改变
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    // 没有改变
+    fn approve(self: Box<Self>) -> Box<dyn State> {
         self
     }
 }
